1f9cdacf80ec886226f543699cc5fb036b2752b0
Nazarii <nazariikubik@gmail.com>
2022-07-30 16:46:27 +0200

    add Header

---
+++ components/Header.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import React from "react";
+import styles from "../styles/_Header.module.scss";
+
+export const Header = () => {
+  return (
+    <div className={styles.header}>
+      <Image
+        alt="dog"
+        src={"/images/dogHeader.jpg"}
+        layout="fill"
+        objectFit="cover"
+        className={styles.img}
+      />
+      <div className={styles.wrapper}>
+        <h3 className={styles.subtitle}>Welcome To</h3>
+        <h1 className={styles.title}>PET STYLE</h1>
+        <a href="#about" className={styles.button}>
+          ABOUT US
+        </a>
+      </div>
+    </div>
+  );
+};
